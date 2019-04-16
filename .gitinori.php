4c8e98c43e1539b8e7e142444b80ed826b2fb526
karolisli <karolisliesys@gmail.com>
2019-04-16 16:18:13 +0300

    .GIT fix

---
+++ .gitinori.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>As,<?php echo date("l")?> ir php</title>
+    </head>
+    <body>
+        <h1>Karolis</h1>- HTML ir PHP asas jau nuo 
+            <?php print date("Y");?> metu
+        <p>Viskas prasidejo </p><?php print date("m");?> menesio,
+            <?php print date("d");?> diena!
+        
+    </body>      
+</html>
