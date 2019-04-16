c7aac0942bca03bacf0be01c3a9c230374d339b0
karolisli <karolisliesys@gmail.com>
2019-04-16 14:58:12 +0300

    P.P.

---
+++ As vadau HTML.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>As valdau HTML</title>
+    </head>
+    <body>
+        <?php
+        Karolis 
+        ?>
+        <h1>Karolis</h1>
+    </body>
+</html>
