9c27a252c4807e1f0c54ea10a93e655072d96828
karolisli <karolisliesys@gmail.com>
2019-04-16 16:59:04 +0300

    Uzduotis #4

--- Index.php
+++ Index.php
@@ -1,1 +1,11 @@
-<?php echo"1 Body 2 Minds, and they are crap bags"; ?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP lydes ir <?php $d=strtotime('tomorrow'); print date('d', $d) ;?></title>
+    </head>
+    <body>
+        <h1>Karolis</h1> PHP su manim buvo ir 
+            <?php $d=strtotime('-1 hour'); print date('h', $d) ;?> valanda!
+        <p><?php $d=strtotime('+1 year'); echo date('Y', $d) ;?> uz kalnu!</p>
+    </body>
+</html> 
