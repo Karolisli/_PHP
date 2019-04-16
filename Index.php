ca0138d1c989ed10c45148f8f8a4554c88bae70c
karolisli <karolisliesys@gmail.com>
2019-04-16 17:29:16 +0300

    Uzduotis #5

--- Index.php
+++ Index.php
@@ -1,11 +1,19 @@
+<?php date_default_timezone_set('Europe/Vilnius') ;?>
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>PHP lydes ir <?php $d=strtotime('tomorrow'); print date('d', $d) ;?></title>
+        <title>Komentarai</title>
     </head>
     <body>
-        <h1>Karolis</h1> PHP su manim buvo ir 
-            <?php $d=strtotime('-1 hour'); print date('h', $d) ;?> valanda!
-        <p><?php $d=strtotime('+1 year'); echo date('Y', $d) ;?> uz kalnu!</p>
+    <h1>Dainu tekstai <!--BLABLABLA-->
+        
+    <!--BLABLABLA-->
+    <h2>Paragrafo daina</h2>     
+    <!--BLABLABLA-->
+    <p>
+        as toks vienisas jaunas paragrafas...<!--BLABLABLA-->
+    </p>
+    <p> mindau svetimo kodo gatves<!--BLABLABLA--></p>
     </body>
 </html> 
