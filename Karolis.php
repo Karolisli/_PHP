9885fddcb118eecc71da9fcecf5ce8eeefd43e5c
karolisli <karolisliesys@gmail.com>
2019-04-16 15:04:29 +0300

    Fix

---
+++ Karolis.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>As valdau HTML</title>
+    </head>
+    <body>
+        <h1>Karolis</h1>
+        <p>Moku kurti statini turini</p> 
+    </body>
+</html>
