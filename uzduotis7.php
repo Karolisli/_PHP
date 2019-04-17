da1e2783b2b848e1922b6b8d5a796785b287a3b3
karolisli <karolisliesys@gmail.com>
2019-04-17 08:49:53 +0300

    Uzduotis #7

---
+++ uzduotis7.php
@@ -0,0 +1,12 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP lyses ir <?php echo rand(strtotime('year'), strtotime('+10year')) ?></title>
+    </head>
+    <body>
+        <h1>Vardas - Galbut turesiu <?php rand(1, 5)
+        ?>vaiku(us)!</h1>
+        <p>D.Trump'as nebebus prezidentu</p> <?php echo rand('Y'),date(m,d) ;?>
+    </body>
+</html>
