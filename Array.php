fea215553049b983e32968f446691650a5bb52af
Karolis <karolisliesys@gmail.com>
2019-04-25 15:06:13 +0300

    array1

---
+++ Array.php
@@ -0,0 +1,16 @@
+<?php
+
+    $array = ['oboulys','apelsinas','cukinija'];
+            var_dump($array);
+   
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Array</title>
+<!--        <link rel="stylesheet" type="text/css" href="css/style.css">-->
+    </head>
+    <body>
+  
+    </body>
+</html>
