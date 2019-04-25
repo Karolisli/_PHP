071e9b6daaad1ecb494c51320a29db9ffd95a824
Karolis <karolisliesys@gmail.com>
2019-04-25 13:45:16 +0300

    Grikiai

---
+++ 19-04-24 grikiai2.php
@@ -0,0 +1,25 @@
+<?php
+
+$grikiai_start = 5000;
+$praejo_dienu = 0;
+
+for ($grikiai = $grikiai_start; $grikiai > 0; $grikiai -= rand(200, 500)) {
+    $praejo_dienu++;
+    //print "suvalgysiu $per_diena ir liks $grikiai grikiu <br>";
+}
+
+$date = date('Y-M-d', strtotime("+$praejo_dienu days"));
+$text = "Jei turesiu $grikiai_start g. grikiu, valgysiu juos $praejo_dienu dienu."
+        . " Jei pradesiu siandien, grikiai baigsis $date data";
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ciklai</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <p><?php print $text; ?></p>
+    </body>
+</html>
