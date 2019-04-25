56ed770df703675165e469df30d08ede5ea4348c
Karolis <karolisliesys@gmail.com>
2019-04-25 16:07:24 +0300

    array2

--- Array.php
+++ Array.php
@@ -1,16 +1,42 @@
 <?php
 
-    $array = ['oboulys','apelsinas','cukinija'];
-            var_dump($array);
-   
+$array = [
+    'kiausiniai' => [
+        'name' => 'Kiaušinis',
+        'amount' => 1,
+        'size' => 'didelis',
+    ],
+    'baklazanai' => [
+        'name' => 'Baklažanas',
+        'amount' => 1,
+        'size' => 'mazas',
+    ],
+    'grietine' => [
+        'name' => 'Grietinė',
+        'amount' => 1,
+        'size' => 'mazas',
+    ]
+];
+
+var_dump($array);
+
 ?>
-<!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
         <title>Array</title>
-<!--        <link rel="stylesheet" type="text/css" href="css/style.css">-->
     </head>
     <body>
-  
+        <div class="produktas">
+            <span class="pavadinimas">
+                <?php print $array['grietine']['name']; ?>
+            </span>
+            <span class="kiekis">
+                <?php print $array['grietine']['amount']; ?>   
+            </span>
+            <span class="dydis">
+                <?php print $array['grietine']['size']; ?>
+            </span>
+        </div>
     </body>
 </html>
