177e9bba7b79263aee3ade07a1b32dbdde2fedf6
Karolis <karolisliesys@gmail.com>
2019-05-02 15:19:23 +0300

    19-05-02 function5

---
+++ 19-05-02start3.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * generuoja dinamine matrica
+ * @param integer $size matricos dydis
+ * @return type
+ */
+function slot_box($size) {
+    $array = [];
+    for ($x = 0; $x <= $size; $x++) {
+        $row = [];
+        for ($z = 0; $z <= $size; $z++) {
+            $row[] = rand(0, 1);
+        }
+        $array[] = $row;
+    }
+    return $array;
+}
+
+$size = rand(1, 5);
+var_dump(slot_box($size));
+
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Slot-box</title>
+    </head>
+    <body>
+        <?php
+// put your code here
+        ?>
+    </body>
+</html>
+
