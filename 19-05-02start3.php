3710cc327848c2904eed5faaccf16a44454ac1d6
Karolis <karolisliesys@gmail.com>
2019-05-02 15:45:18 +0300

    19-05-02 function7

--- 19-05-02start3.php
+++ 19-05-02start3.php
@@ -7,29 +7,61 @@
  */
 function slot_box($size) {
     $array = [];
+    
     for ($x = 0; $x <= $size; $x++) {
         $row = [];
+        
         for ($z = 0; $z <= $size; $z++) {
             $row[] = rand(0, 1);
         }
+        
         $array[] = $row;
     }
     return $array;
 }
 
-$size = rand(1, 5);
+$size = 2;
 var_dump(slot_box($size));
+$matrix = (slot_box($size));
 
 ?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Slot-box</title>
+        <style>
+            div {
+                border: 3px solid white;
+                padding: 40px;
+                margin: 1px;
+            }
+            
+            section {
+                display: flex;
+                justify-content: center;
+                align-items: baseline;
+            }
+            
+            .blue {
+                background-color: blue;
+            }
+            
+            .orange {
+                background-color: orange;
+            }
+        </style>
     </head>
     <body>
-        <?php
-// put your code here
-        ?>
+            <?php foreach ($matrix as $row): ?>
+            <section>
+                <?php foreach ($row as $col): ?>
+                    <?php if ($col > 0): ?>
+                        <div class="blue"></div>
+                    <?php else: ?>
+                        <div class ="orange"></div>
+                    <?php endif; ?>
+            <?php endforeach; ?>
+            </section>
+<?php endforeach; ?>
     </body>
 </html>
-
